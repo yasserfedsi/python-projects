eec682dff7f83b48c1f7641c25c9d0c1de4b3f8b
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-03 20:59:33 +0100

    Update main.py

---
+++ Calculate-Average/main.py
@@ -0,0 +1,13 @@
+def count(P, Q):
+    return P + Q
+
+average = 0
+
+print("Welcome to average calculator")
+
+num_1 = int (input("Please enter your average of the first trimester: "))
+num_2 = int (input("Please enter your average of the second trimester: "))
+num_3 = int (input("Please enter your average of the third trimester: "))
+
+if (num_1, num_2, num_3):
+    print(num_1 + num_2 + num_3, count )
