95954ce3e6c58010df4db0db48a508e369debaf4
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-04 10:35:59 +0100

    Some changes

--- Calculate-Average/main.py
+++ Calculate-Average/main.py
@@ -1,8 +1,15 @@
-num_1 = int (input("enter first no: "))
-num_2 = int (input("enter second no: "))
-num_3 = int (input("enter third no: "))
+num_1 = int (input("enter no: "))
+num_2 = int (input("enter no: "))
+num_3 = int (input("enter no: "))
+num_4 = int (input("enter no: "))
+num_5 = int (input("enter no: "))
+num_6 = int (input("enter no: "))
+num_7 = int (input("enter no: "))
+num_8 = int (input("enter no: "))
+num_9 = int (input("enter no: "))
+num_10 = int (input("enter no: "))
 
-add = num_1 + num_2 + num_3
+add = num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8 + num_9 + num_10
 print("Addition is: ", add)
 
 average = add / 10
