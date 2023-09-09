0d1071030215b24d3152f506a5e2a0296e12cf1f
TheGiantNinja <yasserfedsi34@gmail.com>
2023-09-09 21:53:46 +0200

    Done

--- contactBook.py
+++ contactBook.py
@@ -10,19 +10,20 @@ class ContactBook:
 
     def add_contact(self, contact):
         self.contacts.append(contact)
-        print("Contact added successfully.")
+        print("Contact added successfully!")
 
-    def display_contact(self):
-        print("======Contacts======")
+    def display_contacts(self):
+        print("--- Contacts ---")
         for contact in self.contacts:
             print(f"Name: {contact.name}")
             print(f"Phone: {contact.phone_number}")
             print(f"Email: {contact.email}")
+            print()
 
     def search_contact(self, name):
         for contact in self.contacts:
             if contact.name.lower() == name.lower():
-                print("-- Contact Found --")
+                print("--- Contact Found ---")
                 print(f"Name: {contact.name}")
                 print(f"Phone: {contact.phone_number}")
                 print(f"Email: {contact.email}")
@@ -32,17 +33,48 @@ class ContactBook:
     def delete_contact(self, name):
         for contact in self.contacts:
             if contact.name.lower() == name.lower():
-               self.contacts.remove(contact)
-               return
+                self.contacts.remove(contact)
+                print("Contact deleted successfully!")
+                return
         print("Contact not found.")
 
 def main():
-    contact_book = ContactBook
-    
+    contact_book = ContactBook()
+
     while True:
-        print("======Conatct Book======")
-        print("1. Add contact")
-        print("2. Display contacts")
-        print("3. Search contact.")
-        print("4. Delete contact.")
+        print("======= Contact Book =======")
+        print("1. Add Contact")
+        print("2. Display Contacts")
+        print("3. Search Contact")
+        print("4. Delete Contact")
         print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            name = input("Enter name: ")
+            phone_number = input("Enter phone number: ")
+            email = input("Enter email: ")
+            contact = Contact(name, phone_number, email)
+            contact_book.add_contact(contact)
+
+        elif choice == "2":
+            contact_book.display_contacts()
+
+        elif choice == "3":
+            search_name = input("Enter name to search: ")
+            contact_book.search_contact(search_name)
+
+        elif choice == "4":
+            delete_name = input("Enter name to delete: ")
+            contact_book.delete_contact(delete_name)
+
+        elif choice == "5":
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+if __name__ == "__main__":
+    main()
